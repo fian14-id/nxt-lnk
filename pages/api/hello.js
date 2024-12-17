f036e010b97f995a2ba93f857ee511ce0c3b0554
fian14-id <allifian051@gmail.com>
2024-12-17 10:58:40 +0700

    Revert "add description of Jane Doe ZZZ"
    This reverts commit d119e12ba755ae713f3543aff34532965853b0dd.

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,8 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-    res.status(200).json({
-        name: 'Jane Doe zzz🤤',
-        desc: 'Jane Doe (S, Icon Physical Physical, Icon Anomaly Anomaly), is a playable Agent in Zenless Zone Zero. She is a criminal behavior specialist working as a consultant for the Criminal Investigation Special Response Team.'
-    })
+    res.status(200).json({ name: 'Jane Doe zzz🤤' })
 }
